817e9aa7c8019ee27da7ee2ed597bb73b36bef91
Yiren Zhou <yiren.chow@gmail.com>
2020-05-05 16:38:54 -0400

    changed video description style

--- src/components/Content/Videos/Videos.js
+++ src/components/Content/Videos/Videos.js
@@ -10,10 +10,15 @@ const content = [
         allow:
             "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
         title: "Importance of Synchronisation",
-        description: `In a high-school English presentation back in 2014, 
-            I discussed the my personal video editing experience 
-            and made this to showcase the power of syncing audio with graphics. 
-            This was also my first time of using Cinema4D to create 3D text objects to augment the reality.`,
+        description: (
+            <p>
+                In a high-school English presentation back in 2014, I discussed
+                the my personal video editing experience and made this to
+                showcase the power of syncing audio with graphics. This was also
+                my first time of using <em>Cinema4D</em> to create 3D text
+                objects to augment the reality.
+            </p>
+        ),
     },
     {
         src: "https://www.youtube.com/embed/_UZVw0Uk9Z0",
@@ -21,9 +26,17 @@ const content = [
         allow:
             "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
         title: "Being by Kevin Brooks - A Personification",
-        description: `I read a novel called "Being" by Kevin Brooks - a science fiction about a boy who "discovers he's not one hundred percent human". 
-            I thought it would be great to make an animated video about it. I was into "Team Fortress 2" at the time and decided to use characters from
-            the game to personify the novel. I ended up spending 20 hours learning the Source Filmmaker just for making this video!`,
+        description: (
+            <p>
+                In high school, I read a science fiction called "Being" by Kevin
+                Brooks about a boy who "discovers he's not one hundred percent
+                human". I thought it would be great to make an animated video
+                about it. I was into "Team Fortress 2" at the time and decided
+                to use characters from the game to personify the novel. I ended
+                up spending <em>20 hours</em> learning the{" "}
+                <em>Source Filmmaker</em> just for making this video!
+            </p>
+        ),
     },
 ];
 
